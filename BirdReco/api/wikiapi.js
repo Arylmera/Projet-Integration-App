afdcb8d10c0c2b384fd11343b9cffe70bec13e70
Arylmera <guillaume@lemer.be>
2020-10-04 12:56:36 +0200

    styling et recherche de data
    mise en place d'un bouton en haut a gauche de la page pour le retour ainsi que une meilleur gestion de la taille de l'image

--- BirdReco/api/wikiapi.js
+++ BirdReco/api/wikiapi.js
@@ -1,7 +1,14 @@
 const url = "https://fr.wikipedia.org/api/rest_v1/page/summary/"
+const urlSandBox = "https://fr.wikipedia.org/w/api.php?action=parse&format=json&prop=text|categories|images|sections|wikitext&formatversion=2&page="
 
 export function getWikiInfo(name){
     return fetch(url + name )
+        .then((response) => response.json())
+        .catch((error) => console.log(error))
+}
+
+export function getWikiSandBox(name){
+    return fetch(urlSandBox + name)
         .then((response) => response.json())
         .catch((error) => console.log(error))
 }
--- BirdReco/components/View/details/detailOiseaux.js
+++ BirdReco/components/View/details/detailOiseaux.js
@@ -1,17 +1,24 @@
 import React from 'react'
-import { StyleSheet, View, Text, TouchableOpacity, Image, ScrollView } from 'react-native'
-import {getWikiInfo} from "../../../api/wikiapi";
+import { StyleSheet, View, Text, TouchableOpacity, Image, ScrollView, ActivityIndicator } from 'react-native'
+import {getWikiInfo, getWikiSandBox} from "../../../api/wikiapi";
 import {useNavigation} from "@react-navigation/core";
-import {SafeAreaView} from "react-native-web";
+
+import {FontAwesomeIcon} from "@fortawesome/react-native-fontawesome";
+import { faAngleLeft } from '@fortawesome/free-solid-svg-icons'
 
 class DetailOiseaux extends React.Component {
 
     constructor(props){
         super(props);
         this.state={
+            windowW: 400,
+            windowH: 400,
             oiseaux_nom : this.props.route.params.oiseaux_nom,
             wikiInfo:[],
-            image : " "
+            wikiSandBoxInfo: [],
+            image : " ",
+            imageH : 0,
+            isLoading : true
         }
         this._loadinfo();
     }
@@ -19,34 +26,44 @@ class DetailOiseaux extends React.Component {
     _loadinfo(){
         getWikiInfo(this.state.oiseaux_nom).then(data => {
             if(data) {
-                console.log(data)
-                this.setState({wikiInfo: data, image: data.originalimage.source });
+                //console.log(data);
+                this.setState({wikiInfo: data, image: data.originalimage.source, imageH : data.originalimage.height});
+                this.setState( {isLoading : false} )
             }
         });
     }
 
+    _find_dimesions(layout){
+        const {width, height} = layout;
+        this.setState({windowW: width, windowH: height});
+    }
+
     render() {
         const { navigation } = this.props
+
         return (
             <View style={styles.main_container}>
-                <ScrollView>
-                    <View style={styles.header_container}>
-                        <Image style={styles.image} source={{uri : this.state.image }}/>
+                <TouchableOpacity
+                    style={styles.touchableOpacity}
+                    onPress={() => navigation.navigate('SearchView') }>
+                    <FontAwesomeIcon icon = {faAngleLeft} size={25}/>
+                </TouchableOpacity>
+                <ScrollView style={styles.scrollView_container} key={this.state.windowW} onLayout={(event) => { this._find_dimesions(event.nativeEvent.layout) }}>
+                    <View style={[styles.header_container]}>
+                        <Image style={[styles.image, {height: this.state.windowW}]} source={{uri : this.state.image }}/>
                         <Text style={styles.Title}>{this.state.wikiInfo.displaytitle}</Text>
                     </View>
-                    <Text>{this.state.wikiInfo.extract}</Text>
                     <View style={styles.body_container}>
-
-                    </View>
-                    <View style={styles.footer_container}>
-                    <TouchableOpacity
-                        style={styles.touchableOpacity}
-                        onPress={() => navigation.navigate('SearchView') }
-                    >
-                        <Text>Retour</Text>
-                    </TouchableOpacity>
+                        <Text>{this.state.wikiInfo.extract}</Text>
+                        {this.state.wikiSandBoxInfo}
                     </View>
                 </ScrollView>
+                { this.state.isLoading ?
+                    <View style={styles.loading_container}>
+                        <ActivityIndicator size='large' />
+                    </View>
+                    : null
+                }
             </View>
         )
     }
@@ -55,24 +72,18 @@ class DetailOiseaux extends React.Component {
 const styles = StyleSheet.create({
     main_container: {
         flex: 1,
-        marginTop: 10,
-        marginLeft: 10,
-        marginRight: 10,
         alignItems: 'center',
         justifyContent: 'center'
     },
+    scrollView_container: {
+        marginTop: 35
+    },
     header_container: {
         alignItems: 'center',
-        height: 200,
         marginBottom: 20,
     },
     body_container: {
-        width: '100%',
-        marginTop: 15,
         margin: 10,
-    },
-    footer_container: {
-
     },
     Title : {
         fontSize: 30,
@@ -80,16 +91,37 @@ const styles = StyleSheet.create({
     },
     touchableOpacity: {
         borderRadius: 5,
-        width: "25%",
-        padding: 3,
         alignItems: "center",
-        backgroundColor: "rgba(126,211,33,0.5)"
+        backgroundColor: "rgba(126,211,33,0.5)",
+        position: "absolute",
+        left: 5,
+        top: 5,
     },
     image: {
         flex: 1,
         resizeMode: "contain",
         height: "100%",
         width: "100%",
+        ...Platform.select({
+            ios: {
+                shadowColor: 'rgba(0,0,0, .7)',
+                shadowOffset: { height:0, width: 0 },
+                shadowOpacity: 1,
+                shadowRadius: 5,
+            },
+            android: {
+                elevation: 5
+            },
+        }),
+    },
+    loading_container: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 100,
+        bottom: 0,
+        alignItems: 'center',
+        justifyContent: 'center'
     }
 })
 
