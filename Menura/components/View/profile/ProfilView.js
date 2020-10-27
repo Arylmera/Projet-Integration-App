fb93c5300e432f41dbaa54f8f6fe03e5fce6378c
Amaury <amaurycrenier@gmail.com>
2020-10-27 18:02:59 +0100

    modif mdp et delete account
    mis een place des options de profil:
    modifier mot de passe
    suppression de compte

--- Menura/components/View/profile/ProfilView.js
+++ Menura/components/View/profile/ProfilView.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, View, Text, Image, TouchableOpacity, ScrollView } from 'react-native'
+import { StyleSheet, View, Text, Image, TouchableOpacity, ScrollView, Modal, TextInput } from 'react-native'
 import {useNavigation} from "@react-navigation/core";
 import {connect} from "react-redux"
 import firebase from "firebase";
@@ -14,7 +14,10 @@ class ProfilView extends React.Component {
             nom: "User lastName",
             email: "email",
             profileIcon: "../../assets/images/profileIcon.png",
-            password: ""
+            password: "",
+            newPassword: "",
+            updatePasswordModal: false,
+            deleteAccountModal: false
         }
     }
 
@@ -52,6 +55,22 @@ class ProfilView extends React.Component {
             })
     }
 
+    _passwordTextInputChanged(password) {
+        this.setState({password: password})
+    }
+
+    _newPasswordTextInputChanged(newPassword) {
+        this.setState({newPassword: newPassword})
+    }
+
+    _askDeleteAccount() {
+        this.setState({deleteAccountModal: true})
+    }
+
+    _askUpdatePassword() {
+        this.setState({updatePasswordModal: true})
+    }
+
     _deleteAccount() {
         const user = firebase.auth().currentUser
         const credential = firebase.auth.EmailAuthProvider.credential(
@@ -64,7 +83,28 @@ class ProfilView extends React.Component {
         user.delete()
             .then(() => {
                 console.log("compte supprimé")
+                this.setState({deleteAccountModal: false})
                 this.props.navigation.navigate('ConnexionProfil')
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }
+
+    _updatePassword() {
+        const user = firebase.auth().currentUser
+        const credential = firebase.auth.EmailAuthProvider.credential(
+            user.email,
+            this.state.password
+        );
+        user.reauthenticateWithCredential(credential)
+            .then(() => console.log("authentifié !"))
+            .catch((error) => console.log(error))
+        user.updatePassword(this.state.newPassword)
+            .then(() => {
+                console.log("mot de passe modifié")
+                this.setState({updatePasswordModal: false})
+                this.props.navigation.navigate('Profil')
             })
             .catch((error) => {
                 console.log(error)
@@ -85,19 +125,36 @@ class ProfilView extends React.Component {
                 </View>
                 <View style={styles.container_row}>
                     <View style={styles.body_container}>
-                        <View style={styles.body_info}>
-                            <Text>{this.state.email}</Text>
-                        </View>
-                        <View style={styles.body_footer}>
-                        <TouchableOpacity
-                            style={[styles.modifButton, {backgroundColor: theme.secondary}]}
-                            onPress={() => navigation.navigate('ModidifProfil', { params: {} }) }
-                        >
-                            <Text>Modifier</Text>
-                        </TouchableOpacity>
-                        </View>
+                        <Text>{this.state.email}</Text>
                     </View>
                 </View>
+                <TouchableOpacity
+                    style={[styles.modifButton, {backgroundColor: theme.secondary}]}
+                    onPress={() => this._askUpdatePassword() }
+                >
+                    <Text>modifier mot de passe</Text>
+                </TouchableOpacity>
+                <Modal visible={this.state.updatePasswordModal}>
+                    <Text style={[styles.text]}>Mot de passe actuel:</Text>
+                    <TextInput
+                        style={[styles.textinput, {marginTop: 20}]}
+                        placeholder="mot de passe"
+                        secureTextEntry={true}
+                        onChangeText={(password) => this._passwordTextInputChanged(password)}
+                    />
+                    <Text style={[styles.text]}>Nouveau mot de passe:</Text>
+                    <TextInput
+                        style={[styles.textinput, {marginTop: 20}]}
+                        placeholder="mot de passe"
+                        onChangeText={(newPassword) => this._newPasswordTextInputChanged(newPassword)}
+                    />
+                    <TouchableOpacity
+                        style={[styles.modifButton, {backgroundColor: "#0084d1"}]}
+                        onPress={() => this._updatePassword() }
+                    >
+                        <Text>modifier mon mot de passe</Text>
+                    </TouchableOpacity>
+                </Modal>
                 <TouchableOpacity
                     style={[styles.modifButton, {backgroundColor: theme.secondary}]}
                     onPress={() => this._logOut() }
@@ -106,10 +163,26 @@ class ProfilView extends React.Component {
                 </TouchableOpacity>
                 <TouchableOpacity
                     style={[styles.modifButton, {backgroundColor: theme.secondary}]}
-                    onPress={() => this._deleteAccount() }
+                    onPress={() => this._askDeleteAccount() }
                 >
                     <Text>supprimer compte</Text>
                 </TouchableOpacity>
+                <Modal visible={this.state.deleteAccountModal}>
+                    <Text style={[styles.text]}>Attention cette action supprime définitivement votre compte !</Text>
+                    <Text style={[styles.text]}>Pour continuer, entrez votre mot de passe :</Text>
+                    <TextInput
+                        style={[styles.textinput, {marginTop: 20}]}
+                        placeholder="mot de passe"
+                        secureTextEntry={true}
+                        onChangeText={(password) => this._passwordTextInputChanged(password)}
+                    />
+                    <TouchableOpacity
+                        style={[styles.modifButton, {backgroundColor: "#de0404"}]}
+                        onPress={() => this._deleteAccount() }
+                    >
+                        <Text>Supprimer mon compte</Text>
+                    </TouchableOpacity>
+                </Modal>
             </ScrollView>
         )
     }
@@ -160,9 +233,23 @@ const styles = StyleSheet.create({
         marginRight: "auto",
         marginTop: 10,
         borderRadius: 5,
-        width: "35%",
+        width: "50%",
         padding: 3,
         alignItems: "center",
+    },
+    textinput: {
+        marginLeft: 5,
+        marginRight: 5,
+        marginTop: 5,
+        height: 50,
+        borderWidth: 5,
+        borderRadius: 10,
+        paddingLeft: 10
+    },
+    text: {
+        marginLeft: 5,
+        marginRight: 5,
+        marginTop: 40
     }
 })
 
