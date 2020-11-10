212c72cf9a4076430e8afe7b35ab3dad1602b717
Amaury <amaurycrenier@gmail.com>
2020-11-10 17:52:20 +0100

    fix historique request + lien doc API readme

--- MenuraTest/api/historique_api.js
+++ MenuraTest/api/historique_api.js
@@ -1,19 +1,19 @@
 const url_historique_byID =
-  'http://146.59.195.248:3000/v1/historiques?utilisateur=';
+  'http://146.59.195.248:3000/v1/api/historiques?utilisateur=';
 
 /**
  *
  * @param id
  * @return {Promise<Response | void>}
  */
-export function getHistoriqueByID(id, idToken) {
-  return fetch(url_historique_byID + id, {
+export function getHistoriqueByID(utilisateur, idToken) {
+  return fetch(url_historique_byID + utilisateur, {
     headers: {
       Accept: 'application/json',
       'Content-Type': 'application/json',
       Authorization: 'Bearer ' + idToken,
     },
   })
-    .then((response) => response)
+    .then((response) => response.json())
     .catch((error) => console.log(error));
 }
