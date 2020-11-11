99fa41a58d66fa11db53d3f99eb19dafc90d1f67
Arylmera <guillaume@lemer.be>
2020-11-11 15:45:18 +0100

    Update Historique + capterus paramètres

--- MenuraTest/functions/capteur.js
+++ MenuraTest/functions/capteur.js
@@ -6,40 +6,47 @@ import {
   TouchableOpacity,
   FlatList,
   TextInput,
+  SafeAreaView,
 } from 'react-native';
 import {connect} from 'react-redux';
 import {Divider} from 'react-native-paper';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import Capteur_Item from './capteurItem';
 import firebase from 'firebase';
-import {getHistoriqueByID} from '../api/historique_api';
+import {addCapteur, getCapteurListById} from '../api/capteur_api';
 
 class Capteur extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      capteur_list: [
-        {id: 0, name: 'testing'},
-        {id: 1, name: 'testing1'},
-        {id: 2, name: 'testing2'},
-      ],
+      updater: false,
+      user: {},
+      capteur_list: '',
       user_id: '',
       mac_add_capteur: '',
       mac_address_message: '',
       status_text: 'Aucun capteur lié',
     };
+    this.mac_add_input = React.createRef();
   }
 
+  /**
+   * au chargement du component
+   */
   componentDidMount() {
     this._checkIfLoggedIn();
     if (this.state.user_id !== '') {
     }
   }
 
+  /**
+   * check du login utilisateur et récupération des informations
+   * @private
+   */
   _checkIfLoggedIn() {
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
-        this.setState({user_id: user.uid});
+        this.setState({user_id: user.uid, user: user});
         this.load_capteur_list(user);
       } else {
         console.log('no user');
@@ -47,24 +54,50 @@ class Capteur extends React.Component {
     });
   }
 
+  /**
+   * récupération de la liste des capteur de l'utilisateur par la requete get
+   * @param user
+   */
   load_capteur_list(user) {
-    console.log('loading capteur list for user : ' + this.state.user_id);
+    this.setState({list_caption: 'Chargement de vos capteurs'});
     user.getIdToken(true).then((idToken) => {
-      getHistoriqueByID(this.state.user_id, idToken).then((data) =>
-        console.log(data), // set state capteur_list
+      getCapteurListById(this.state.user_id, idToken).then((data) =>
+        this.setState({capteur_list: data.data}),
       );
     });
   }
 
+  reload_list() {
+    console.log('reload');
+    this.setState({updater: !this.state.updater});
+    this.load_capteur_list(this.state.user);
+  }
+
+  /**
+   * helper textInput changement de la macAdresse
+   * @param mac_adresse
+   * @private
+   */
   _mac_adresse_change(mac_adresse) {
     this.setState({mac_add_capteur: mac_adresse});
   }
 
+  /**
+   * ajout d'un capteur
+   * vérification de la valeur de la mac adresse
+   * envois par requete POST a la base de donnée
+   * @private
+   */
   _add_capteur() {
     let Mac_Regex = /^(([A-Fa-f0-9]{2}[:]){5}[A-Fa-f0-9]{2}[,]?)+$/i;
     if (Mac_Regex.test(this.state.mac_add_capteur)) {
-      console.log(this.state.mac_add_capteur + ' is a mac addresse');
-      // Ajout du capteur dans la DB lié a l'utilisateur
+      this.state.user.getIdToken(true).then((idToken) => {
+        addCapteur(this.state.user_id, idToken, this.state.mac_add_capteur)
+          .then(this.mac_add_input.current.clear())
+          .then(this.load_capteur_list(this.state.user))
+          .then(this.reload_list)
+          .catch((e) => console.log(e));
+      });
     } else {
       console.log('error on mac addresse');
       this.setState({
@@ -73,71 +106,87 @@ class Capteur extends React.Component {
     }
   }
 
+  /**
+   * render
+   * @return {JSX.Element}
+   */
   render() {
     let theme = this.props.currentStyle;
+    console.log(this.state.capteur_list);
     return (
-      <View style={[styles.container]}>
-        <View style={[styles.container_header]}>
-          <Text style={[styles.title, {color: theme.highlight}]}>
-            Capteurs liés
-          </Text>
-          <View
-            style={[
-              styles.add_capteur_container,
-              {backgroundColor: theme.accent, color: theme.highlight},
-            ]}>
-            <Text style={{color: theme.highlight}}>Adresse MAC :</Text>
-            <TextInput
-              placeholder="FF:FF:FF:FF:FF:FF"
-              autocorrect={false}
-              onChangeText={(mac_adresse) =>
-                this._mac_adresse_change(mac_adresse)
-              }
-              style={[styles.add_capteur_input, {color: theme.highlight}]}
-            />
-            <TouchableOpacity
-              onPress={this._add_capteur.bind(this)}
+      <SafeAreaView style={{flex: 1}}>
+        <View style={[styles.container]}>
+          <View style={[styles.container_header]}>
+            <Text style={[styles.title, {color: theme.highlight}]}>
+              Liste de mes Capteurs
+            </Text>
+            <View
               style={[
-                styles.add_capteur_button,
-                {backgroundColor: theme.accent},
+                styles.add_capteur_container,
+                {backgroundColor: theme.accent, color: theme.highlight},
               ]}>
-              <Icon
-                name="add"
-                size={18}
-                color={theme.highlight}
-                style={{
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                }}
+              <Text style={{color: theme.highlight}}>Adresse MAC :</Text>
+              <TextInput
+                placeholder="FF:FF:FF:FF:FF:FF"
+                autocorrect={false}
+                ref={this.mac_add_input}
+                onChangeText={(mac_adresse) =>
+                  this._mac_adresse_change(mac_adresse)
+                }
+                style={[styles.add_capteur_input, {color: theme.highlight}]}
               />
-            </TouchableOpacity>
-          </View>
-          <Text style={[styles.mac_address_message, {color: theme.highlight}]}>
-            {this.state.mac_address_message}
-          </Text>
-        </View>
-        <Divider />
-        <View style={styles.capteur_list}>
-          {this.state.capteur_list.length > 0 ? (
-            <FlatList
-              data={this.state.capteur_list}
-              style={styles.FlatlistItem}
-              keyExtractor={(item) => item.id}
-              renderItem={({item}) => (
-                <Capteur_Item
-                  data={{
-                    capteur: item,
+              <TouchableOpacity
+                onPress={this._add_capteur.bind(this)}
+                style={[
+                  styles.add_capteur_button,
+                  {backgroundColor: theme.accent},
+                ]}>
+                <Icon
+                  name="add"
+                  size={18}
+                  color={theme.highlight}
+                  style={{
+                    justifyContent: 'center',
+                    alignItems: 'center',
                   }}
                 />
-              )}
-            />
-          ) : (
-            <Text style={[styles.capteur_unknown, {color: theme.highlight}]}>
-              {this.state.status_text}
+              </TouchableOpacity>
+            </View>
+            <Text
+              style={[styles.mac_address_message, {color: theme.highlight}]}>
+              {this.state.mac_address_message}
             </Text>
-          )}
+          </View>
+          <Divider
+            style={[styles.divider, {backgroundColor: theme.highlight}]}
+          />
+          <View style={styles.capteur_list}>
+            {this.state.capteur_list.length > 0 ? (
+              <View>
+                <FlatList
+                  data={this.state.capteur_list}
+                  style={styles.FlatlistItem}
+                  extraData={this.state.reload}
+                  keyExtractor={(item) => item.id}
+                  renderItem={({item}) => (
+                    <Capteur_Item
+                      reload_list={this.reload_list.bind(this)}
+                      data={{
+                        capteur: item,
+                        user: this.state.user,
+                      }}
+                    />
+                  )}
+                />
+              </View>
+            ) : (
+              <Text style={[styles.capteur_unknown, {color: theme.highlight}]}>
+                {this.state.status_text}
+              </Text>
+            )}
+          </View>
         </View>
-      </View>
+      </SafeAreaView>
     );
   }
 }
@@ -148,9 +197,14 @@ const styles = StyleSheet.create({
     minHeight: 200,
     flexDirection: 'column',
   },
+  divider: {
+    margin: 0,
+    padding: 0,
+  },
   container_header: {
     flex: 1,
     alignItems: 'center',
+    justifyContent: 'center',
   },
   title: {
     textAlign: 'center',
@@ -165,7 +219,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     borderRadius: 5,
     marginTop: 5,
-    padding: 5,
+    marginBottom: 0,
     width: '90%',
   },
   add_capteur_button: {
@@ -178,6 +232,7 @@ const styles = StyleSheet.create({
     width: 150,
     marginLeft: 5,
     marginRight: 5,
+    padding: 10,
     textAlign: 'center',
   },
   add_capteur_button_text: {
--- MenuraTest/functions/capteurItem.js
+++ MenuraTest/functions/capteurItem.js
@@ -1,21 +1,62 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
 import {connect} from 'react-redux';
+import {deleteCapteur} from '../api/capteur_api';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 class Capteur_Item extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      updater: false,
+      user: this.props.data.user,
       capteur: this.props.data.capteur,
     };
   }
 
+  componentDidMount() {
+    this.reload_item();
+  }
+
+  reload_item() {
+    this.setState({updater: !this.state.updater});
+  }
+
+  /**
+   * suppression du capteur dans la db par requete POST
+   * @private
+   */
+  _delete_capteur() {
+    this.state.user.getIdToken(true).then((idToken) => {
+      deleteCapteur(idToken, this.state.capteur.macAddress).then(() => {
+        this.props.reload_list();
+      });
+    });
+  }
+
+  /**
+   * render
+   * @return {JSX.Element}
+   */
   render() {
     let theme = this.props.currentStyle;
     return (
       <View style={[styles.main_container, {backgroundColor: theme.accent}]}>
-        <Text style={[styles.capteur_id, {color: theme.highlight}]}>Capteur numéro : {this.state.capteur.id}</Text>
-        <Text style={[styles.capteur_name, {color: theme.highlight}]}>Nom : {this.state.capteur.name}</Text>
+        <Text style={[styles.capteur_id, {color: theme.highlight}]}>
+          <Text style={styles.capteur_id_text}>
+            Capteur :{this.state.capteur.macAddress}
+          </Text>
+        </Text>
+        <TouchableOpacity
+          onPress={this._delete_capteur.bind(this)}
+          style={[styles.deleteButton]}>
+          <View style={styles.delete}>
+            <Text style={[styles.capteur_delete, {color: theme.highlight}]}>
+              Supprimer
+            </Text>
+            <Icon name="delete-forever" size={18} color={theme.highlight} />
+          </View>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -30,11 +71,22 @@ let styles = StyleSheet.create({
     margin: 5,
   },
   capteur_id: {
+    flex: 3,
+    justifyContent: 'center',
+  },
+  capteur_id_text: {
+    flex: 1,
+    fontSize: 12,
+  },
+  delete: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  capteur_delete: {},
+  capteur_changeAddress: {
     flex: 1,
   },
-  capteur_name: {
-    flex: 1,
-  }
+  deleteButton: {},
 });
 
 const mapStateToProps = (state) => {
